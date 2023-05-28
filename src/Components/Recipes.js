7e107be187bf86292b4d69330b30dbcbe6c3d596
Sarah Goewey <sarah.goewey@yahoo.com>
2023-05-28 12:20:29 -0400

    adjusted loginWithToken and changed class to className on login

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../store";
 
 const Recipes = () => {
   const { auth } = useSelector((state) => state);
