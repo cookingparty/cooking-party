9e4578f1d25c45f7bfc2e53d968d8bac5527eddd
Sarah Goewey <sarah.goewey@yahoo.com>
2023-05-28 13:35:24 -0400

    setup web sockets

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Nav from "./Nav";
 import Home from "./Home";
 import Login from "./Login";
@@ -11,29 +11,56 @@ import { Link, Routes, Route } from "react-router-dom";
 
 const App = () => {
   const { auth } = useSelector((state) => state);
+  const prevAuth = useRef(auth);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(loginWithToken());
   }, []);
 
+  useEffect(() => {
+    if (!prevAuth.current.id && auth.id) {
+      console.log("just logged in");
+      window.socket = new WebSocket(
+        window.location.origin.replace("http", "ws")
+      );
+      window.socket.addEventListener("open", () => {
+        window.socket.send(
+          JSON.stringify({ token: window.localStorage.getItem("token") })
+        );
+      });
+      window.socket.addEventListener("message", (ev) => {
+        const message = JSON.parse(ev.data);
+        console.log(message);
+      });
+    }
+    if (prevAuth.current.id && !auth.id) {
+      console.log("logged out");
+      window.socket.close();
+    }
+  }, [auth]);
+
+  useEffect(() => {
+    prevAuth.current = auth;
+  });
+
   return (
     <div>
       <h1>Cooking Party</h1>
-        <div>
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/recipes" element={<Recipes />} />
-            <Route path="/meals" element={<Meals />} />
-            <Route path="/cocktails" element={<Cocktails />} />
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/meals" element={<Meals />} />
+          <Route path="/cocktails" element={<Cocktails />} />
 
-
-            {!!auth.id}
+          {!!auth.id}
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
+        </Routes>
+      </div>
       {/* )} */}
     </div>
   );
