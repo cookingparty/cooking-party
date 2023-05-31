bc208ee63105d7a7c097b031f939e44efa3dff28
Sarah Goewey <sarah.goewey@yahoo.com>
2023-05-31 13:35:04 -0400

    adjusted store slice friends

--- src/store/friends.js
+++ src/store/friends.js
@@ -10,74 +10,64 @@ const friends = (state = [], action) => {
 export const fetchFriends = () => {
   return async (dispatch, getState) => {
     const token = window.localStorage.getItem("token");
-    if (getState().auth.id) {
-      const response = await axios.get("/api/friends", {
-        headers: {
-          authorization: token,
-        },
-      });
-
-      dispatch({
-        type: "SET_FRIENDS",
-        friends: [...response.data.friender, ...response.data.friendee],
-      });
-    }
+    const response = await axios.get("/api/friends", {
+      headers: {
+        authorization: token,
+      },
+    });
+    dispatch({
+      type: "SET_FRIENDS",
+      friends: [...response.data.friender, ...response.data.friendee],
+    });
   };
 };
 
 export const addFriend = (id) => {
   return async (dispatch, getState) => {
     const token = window.localStorage.getItem("token");
-    if (getState().auth.id) {
-      const response = await axios.post(
-        "/api/friends/",
-        { id },
-        {
-          headers: {
-            authorization: token,
-          },
-        }
-      );
-
-      dispatch({
-        type: "SET_FRIENDS",
-        friends: [...response.data.friender, ...response.data.friendee],
-      });
-    }
+    const response = await axios.post(
+      "/api/friends/",
+      { id },
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+    dispatch({
+      type: "SET_FRIENDS",
+      friends: [...response.data.friender, ...response.data.friendee],
+    });
   };
 };
 
 export const updateFriend = (updated) => {
   return async (dispatch, getState) => {
     const token = window.localStorage.getItem("token");
-    if (getState().auth.id) {
-      const response = await axios.put("/api/friends", updated, {
-        headers: {
-          authorization: token,
-        },
-      });
-      dispatch({
-        type: "SET_FRIENDS",
-        friends: [...response.data.friender, ...response.data.friendee],
-      });
-    }
+    const response = await axios.put("/api/friends", updated, {
+      headers: {
+        authorization: token,
+      },
+    });
+    dispatch({
+      type: "SET_FRIENDS",
+      friends: [...response.data.friender, ...response.data.friendee],
+    });
   };
 };
 
 export const unfriend = (id) => {
   return async (dispatch, getState) => {
     const token = window.localStorage.getItem("token");
-    if (getState().auth.id) {
-      const response = await axios.delete(`/api/friends/${id}`, {
-        headers: {
-          authorization: token,
-        },
-      });
-      dispatch({
-        type: "SET_FRIENDS",
-        friends: [...response.data.friender, ...response.data.friendee],
-      });
-    }
+    const response = await axios.delete(`/api/friends/${id}`, {
+      headers: {
+        authorization: token,
+      },
+    });
+    dispatch({
+      type: "SET_FRIENDS",
+      friends: [...response.data.friender, ...response.data.friendee],
+    });
   };
 };
 
