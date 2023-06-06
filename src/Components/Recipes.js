85a4b4ded03de7bb778cd82ea3a819cb25837f14
Rebecca Cherry <120757679+rebeccacherry@users.noreply.github.com>
2023-06-06 17:22:20 -0400

    created IG

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../store";
 import OnlineUsers from "./OnlineUsers";
 import Friends from "./Friends";
 import Chat from "./Chat";
+import Instafeed from 'instafeed.js';
 
 const Recipes = () => {
   const { auth } = useSelector((state) => state);
   const dispatch = useDispatch();
+
+ 
+
+  useEffect(() => {
+    const userFeed = new Instafeed({
+      get: 'user',
+      resolution: 'medium_resolution',
+      limit: 6,
+      accessToken: 'IGQVJWOFR4VzVuSGtQZAjNQQUpCRU12dEp0anNHU1dFbTE3QkxVM0tzSS1fRGVSU0swZA1RJQWVQWmszYTZAibVEtdmdwNFJQRnQtTk50Q1VaQ0s4N19UaUs4ZATlGUDUzQmpJTnRrOGttMXRLOXNqdzg4bQZDZD',
+      target: 'instafeed-container'
+    });
+    userFeed.run();
+  }, [])
+
   return (
     <div>
       <h1>Recipes</h1>
@@ -18,6 +33,8 @@ const Recipes = () => {
         {!!auth.id && <Friends />}
         {!!auth.id && <Chat />}
       </div>
+
+      <div id='instafeed-container'></div>
     </div>
   );
 };
