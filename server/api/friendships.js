ddbaff2323a143fb2370cae0c1ff5a6605ff291f
Sarah Goewey <sarah.goewey@yahoo.com>
2023-06-06 10:10:30 -0400

    removed old code

--- server/api/friendships.js
+++ server/api/friendships.js
@@ -1,42 +1,9 @@
 const express = require("express");
 const app = express.Router();
-const { isLoggedIn } = require("./middleware.js");
 const { Friendship } = require("../db");
 const socketMap = require("../socketMap");
 
 module.exports = app;
-
-/*app.get("/", isLoggedIn, async (req, res, next) => {
-  try {
-    res.send(await req.user.getFriends());
-  } catch (ex) {
-    next(ex);
-  }
-});
-
-app.put("/", isLoggedIn, async (req, res, next) => {
-  try {
-    res.send(await req.user.updateFriend(req.body));
-  } catch (ex) {
-    next(ex);
-  }
-});
-
-app.delete("/:id", isLoggedIn, async (req, res, next) => {
-  try {
-    res.send(await req.user.unfriend(req.params.id));
-  } catch (ex) {
-    next(ex);
-  }
-});
-
-app.post("/", isLoggedIn, async (req, res, next) => {
-  try {
-    res.send(await req.user.addFriend(req.body));
-  } catch (ex) {
-    next(ex);
-  }
-});*/
 
 app.get("/", async (req, res, next) => {
   try {
