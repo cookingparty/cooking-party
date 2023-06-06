10eee5608cdea85bb07bcda7dc92a9b0df04328b
Sarah Goewey <sarah.goewey@yahoo.com>
2023-06-06 12:59:52 -0400

    sockets working for group membership actions

--- server/api/memberships.js
+++ server/api/memberships.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express.Router();
-const { Membership } = require("../db");
+const { Membership, Group, User } = require("../db");
+const socketMap = require("../socketMap");
 
 module.exports = app;
 
@@ -15,6 +16,23 @@ app.get("/", async (req, res, next) => {
 app.post("/", async (req, res, next) => {
   try {
     const membership = await Membership.create(req.body);
+
+    const group = await Group.findByPk(membership.groupId);
+
+    const groupAdminMembership = await Membership.findOne({
+      where: {
+        groupId: group.id,
+        role: "Group Admin",
+      },
+    });
+    const groupAdminId = groupAdminMembership.member_id;
+
+    if (socketMap[groupAdminId]) {
+      socketMap[groupAdminId].socket.send(
+        JSON.stringify({ type: "ADD_MEMBERSHIP", membership })
+      );
+    }
+
     res.status(201).send(membership);
   } catch (ex) {
     next(ex);
@@ -25,6 +43,16 @@ app.post("/", async (req, res, next) => {
 app.delete("/:id", async (req, res, next) => {
   try {
     const membership = await Membership.findByPk(req.params.id);
+
+    if (socketMap[membership.member_id]) {
+      socketMap[membership.member_id].socket.send(
+        JSON.stringify({
+          type: "DELETE_MEMBERSHIP",
+          membershipId: membership.id,
+        })
+      );
+    }
+
     await membership.destroy();
     res.sendStatus(204);
   } catch (ex) {
@@ -36,6 +64,12 @@ app.put("/:id", async (req, res, next) => {
   try {
     const membership = await Membership.findByPk(req.params.id);
     res.send(await membership.update(req.body));
+
+    if (socketMap[membership.member_id]) {
+      socketMap[membership.member_id].socket.send(
+        JSON.stringify({ type: "UPDATE_MEMBERSHIP", membership })
+      );
+    }
   } catch (ex) {
     next(ex);
   }
