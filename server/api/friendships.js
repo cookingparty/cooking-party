6fcde19da7499954b56914fe74e5f6e067f3d9d5
Sarah Goewey <sarah.goewey@yahoo.com>
2023-06-06 12:35:11 -0400

    sockets working for all friendship changes

--- server/api/friendships.js
+++ server/api/friendships.js
@@ -31,6 +31,18 @@ app.post("/", async (req, res, next) => {
 app.delete("/:id", async (req, res, next) => {
   try {
     const friendship = await Friendship.findByPk(req.params.id);
+
+    if (socketMap[friendship.friendee_id]) {
+      socketMap[friendship.friendee_id].socket.send(
+        JSON.stringify({ type: "DELETE_FRIENDSHIP", friendship: friendship.id })
+      );
+    }
+    if (socketMap[friendship.friender_id]) {
+      socketMap[friendship.friender_id].socket.send(
+        JSON.stringify({ type: "DELETE_FRIENDSHIP", friendship: friendship.id })
+      );
+    }
+
     await friendship.destroy();
     res.sendStatus(204);
   } catch (ex) {
@@ -42,6 +54,17 @@ app.put("/:id", async (req, res, next) => {
   try {
     const friendship = await Friendship.findByPk(req.params.id);
     res.send(await friendship.update(req.body));
+
+    if (socketMap[friendship.friendee_id]) {
+      socketMap[friendship.friendee_id].socket.send(
+        JSON.stringify({ type: "UPDATE_FRIENDSHIP", friendship })
+      );
+    }
+    if (socketMap[friendship.friender_id]) {
+      socketMap[friendship.friender_id].socket.send(
+        JSON.stringify({ type: "UPDATE_FRIENDSHIP", friendship })
+      );
+    }
   } catch (ex) {
     next(ex);
   }
