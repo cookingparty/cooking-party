46e73abec7f881d1005fe40b35d61ddc8f49fdca
Rebecca Cherry <120757679+rebeccacherry@users.noreply.github.com>
2023-06-11 17:53:59 -0400

    update to online friends

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -177,6 +177,41 @@ const Nav = () => {
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
+  const friends = friendships
+  .filter(
+    (friendship) =>
+      friendship.friendee_id === auth.id || friendship.friender_id === auth.id
+  )
+  .map((friendship) => {
+    if (friendship.friendee_id === auth.id) {
+      return users.find((user) => user.id === friendship.friender_id);
+    }
+    if (friendship.friender_id === auth.id) {
+      return users.find((user) => user.id === friendship.friendee_id);
+    }
+  });
+
+  const findFriendship = (friendId) => {
+    const friendship = friendships.find(
+      (friendship) =>
+        (friendship.friendee_id === friendId &&
+          friendship.friender_id === auth.id) ||
+        (friendship.friendee_id === auth.id &&
+          friendship.friender_id === friendId)
+    );
+    return friendship;
+  };
+  const confirmedFriend = (user) => {
+    const friend = friends.find((f) => f.id === user.id);
+    if (!!friend && findFriendship(friend.id)) {
+      if (findFriendship(friend.id).status === "CONFIRMED") {
+        return true;
+      }
+    }
+    return false;
+  };
+
   const onlineFriends = onlineUsers.filter((user) => !!confirmedFriend(user));
   
 
