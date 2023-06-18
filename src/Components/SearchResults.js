6b7b785338f19eb7b5f92f711fac2e154feba46e
Angel Gao <angelgao9@gmail.com>
2023-06-18 15:21:49 -0400

    added id prop to search results so we can seed data when link is clicked

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import RecipeCard from './RecipeCard';
+import React from "react";
+import RecipeCard from "./RecipeCard";
 
 function SearchResult({ results }) {
-    if (!results) {
-        return null; 
-      }
-    
-    if (results.length === 0) {
-        return <div>No results found.</div>;
-      }
+  if (!results) {
+    return null;
+  }
+
+  if (results.length === 0) {
+    return <div>No results found.</div>;
+  }
 
   return (
     <div className="recipe-grid">
-{results.map((recipe) => (
-  <RecipeCard
-    key={recipe.id}
-    title={recipe.title}
-    subheader={recipe.sourceName}
-    image={recipe.image}
-    description={recipe.summary}
-    readyInMinutes={recipe.readyInMinutes}
-    serves={recipe.servings}
-    avatar={'F'}
-    avatarColor={'red'}
-  />
-))}
-</div> 
+      {results.map((recipe) => (
+        <RecipeCard
+          key={recipe.id}
+          id={recipe.id}
+          title={recipe.title}
+          subheader={recipe.sourceName}
+          image={recipe.image}
+          description={recipe.summary}
+          readyInMinutes={recipe.readyInMinutes}
+          serves={recipe.servings}
+          avatar={"F"}
+          avatarColor={"red"}
+        />
+      ))}
+    </div>
   );
 }
 
 export default SearchResult;
-
