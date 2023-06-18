4b93037aa63a6280403114ef6f8256e5147063dc
Angel Gao <angelgao9@gmail.com>
2023-06-17 20:12:18 -0400

    revised loginRegister to fix uncontrolled error

--- src/Components/LoginRegister.js
+++ src/Components/LoginRegister.js
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { fetchRecipes } from '../store';
+import { fetchRecipes } from "../store";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { attemptLogin, updateAuth, logout, register } from "../store/auth";
-import Carousel from 'react-material-ui-carousel';
+import Carousel from "react-material-ui-carousel";
 
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-const { recipes} = useSelector(state => state);
+  const { recipes } = useSelector((state) => state);
   const { account } = useParams();
 
   const [credentials, setCredentials] = useState({
@@ -24,14 +24,13 @@ const { recipes} = useSelector(state => state);
     setCredentials({ ...credentials, [ev.target.name]: ev.target.value });
   };
 
-   useEffect(() => {
-  dispatch(fetchRecipes());
-    }
-, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchRecipes());
+  }, [dispatch]);
 
-if (recipes.length === 0) {
-  return <p>Loading recipes...</p>;
-}
+  if (recipes.length === 0) {
+    return <p>Loading recipes...</p>;
+  }
 
   const login = async (ev) => {
     ev.preventDefault();
@@ -55,137 +54,143 @@ if (recipes.length === 0) {
   };
 
   return (
-
-     <Box display="flex" flexDirection="column" alignItems="center" marginBottom='120px'  >
-       <Box
-  sx={{
-    backgroundColor: 'almond',
-    margin: '60px',
-    marginBottom: "80px",
-    border: '40px solid almond',
-    height: '400px', 
-    width: '700px', 
-  }}
->
-<Carousel autoPlay={true} animation="slide" interval={6000}>
-  {recipes.slice(0, 9).map((recipe, index) => (
-    <img
-      key={index}
-      src={recipe.imageURL}
-      alt={`Recipe ${index}`}
-      style={{ width: '700px', height: '400px', objectFit: 'cover' }}
-    />
-  ))}
-</Carousel>
-
-
-  <p
-    style={{
-      textAlign: 'center',
-      marginTop: '0',
-      fontSize: '1.5rem',
-      fontWeight: 'bold',
-    }}
-  
->Featured Recipes</p>
-          </Box>
     <Box
-    display="flex"
-    flexDirection="column"
-    alignItems="center"
-    textAlign="center"
-    marginTop={10}
-    p={2}
-    // maxWidth={600}
-    mx="auto"
-    pt={4}
-    bgcolor="#F5F5F5"
-    sx={{width: '700px'}}
-  >
-    <div>
-      <h2>{account === "login" ? "Login" : "Register"}</h2>
-      <form
-         sx={{
-    backgroundColor: 'almond',
-    paddingBottom: "50px",
-    margin: '60px',
-    marginBottom: "120px",
-    border: '40px solid almond',
-    height: '400px', 
-    width: '700px', 
-  }}
-        onSubmit={account === "login" ? login : _register}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      marginBottom="120px"
+    >
+      <Box
+        sx={{
+          backgroundColor: "almond",
+          margin: "60px",
+          marginBottom: "80px",
+          border: "40px solid almond",
+          height: "400px",
+          width: "700px",
+        }}
       >
-        <TextField
-          label="Username"
-          value={credentials.username || credentials.facebook_username}
-          name="username"
-          onChange={onChange}
-        />
-        <div style={{ marginBottom: "1rem" }} />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          name="password"
-          type="password"
-          value={credentials.password}
-          onChange={onChange}
-        />
+        <Carousel autoPlay={true} animation="slide" interval={6000}>
+          {recipes.slice(0, 9).map((recipe, index) => (
+            <img
+              key={index}
+              src={recipe.imageURL}
+              alt={`Recipe ${index}`}
+              style={{ width: "700px", height: "400px", objectFit: "cover" }}
+            />
+          ))}
+        </Carousel>
 
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          size="large"
-          sx={{
-            mt: 2,
-            color: "#333",
-            backgroundColor: "#F9F6EE",
-            "&:hover": { backgroundColor: "#F5F5F5", color: "#888" },
+        <p
+          style={{
+            textAlign: "center",
+            marginTop: "0",
+            fontSize: "1.5rem",
+            fontWeight: "bold",
           }}
         >
-          {account === "login" ? "Login" : "Register"}{" "}
-        </Button>
-      </form>
-      {account === "login" ? (
-        <Button
-          type="submit"
-          onClick={handleLogin}
-          variant="contained"
-          color="primary"
-          size="large"
-          sx={{
-            mt: 2,
-            color: "#333",
-            backgroundColor: "#F9F6EE",
-            "&:hover": { backgroundColor: "#F5F5F5", color: "#888" },
-          }}
-        >
-          Login with Facebook
-        </Button>
-      ) : null}
-      {account === "login" ? (
-        <p>
-          No account? <br />
-          <Button
-            component={Link}
-            to="/auth/register"
-            variant="contained"
-            color="primary"
-            size="large"
-            sx={{
-              mt: 2,
-              color: "#333",
-              backgroundColor: "#F9F6EE",
-              "&:hover": { backgroundColor: "#F5F5F5", color: "#888" },
-            }}
-          >
-            Sign up here
-          </Button>
+          Featured Recipes
         </p>
-      ) : null}
-    </div>
-    </Box>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        textAlign="center"
+        marginTop={10}
+        p={2}
+        // maxWidth={600}
+        mx="auto"
+        pt={4}
+        bgcolor="#F5F5F5"
+        sx={{ width: "700px" }}
+      >
+        <div>
+          <h2>{account === "login" ? "Login" : "Register"}</h2>
+          <form
+            sx={{
+              backgroundColor: "almond",
+              paddingBottom: "50px",
+              margin: "60px",
+              marginBottom: "120px",
+              border: "40px solid almond",
+              height: "400px",
+              width: "700px",
+            }}
+            onSubmit={account === "login" ? login : _register}
+          >
+            <TextField
+              label="Username"
+              value={
+                credentials.username || credentials.facebook_username || ""
+              }
+              name="username"
+              onChange={onChange}
+            />
+            <div style={{ marginBottom: "1rem" }} />
+            <TextField
+              id="filled-password-input"
+              label="Password"
+              name="password"
+              type="password"
+              value={credentials.password || ""}
+              onChange={onChange}
+            />
+
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              size="large"
+              sx={{
+                mt: 2,
+                color: "#333",
+                backgroundColor: "#F9F6EE",
+                "&:hover": { backgroundColor: "#F5F5F5", color: "#888" },
+              }}
+            >
+              {account === "login" ? "Login" : "Register"}{" "}
+            </Button>
+          </form>
+          {account === "login" ? (
+            <Button
+              type="submit"
+              onClick={handleLogin}
+              variant="contained"
+              color="primary"
+              size="large"
+              sx={{
+                mt: 2,
+                color: "#333",
+                backgroundColor: "#F9F6EE",
+                "&:hover": { backgroundColor: "#F5F5F5", color: "#888" },
+              }}
+            >
+              Login with Facebook
+            </Button>
+          ) : null}
+          {account === "login" ? (
+            <p>
+              No account? <br />
+              <Button
+                component={Link}
+                to="/auth/register"
+                variant="contained"
+                color="primary"
+                size="large"
+                sx={{
+                  mt: 2,
+                  color: "#333",
+                  backgroundColor: "#F9F6EE",
+                  "&:hover": { backgroundColor: "#F5F5F5", color: "#888" },
+                }}
+              >
+                Sign up here
+              </Button>
+            </p>
+          ) : null}
+        </div>
+      </Box>
     </Box>
   );
 };
