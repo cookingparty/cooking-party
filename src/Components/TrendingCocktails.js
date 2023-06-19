66c33bd66c0644da65e8938064d29c049d4b1d62
Sarah Goewey <sarah.goewey@yahoo.com>
2023-06-19 14:56:21 -0400

    cocktails 4

--- src/Components/TrendingCocktails.js
+++ src/Components/TrendingCocktails.js
@@ -1,32 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import RecipeCard from './RecipeCard';
-import { apiKeyCocktail } from '../../secrets';
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import RecipeCard from "./RecipeCard";
+import { apiKeyCocktail } from "../../secrets";
 
 function TrendingCocktails() {
-	const [trendingCocktails, setTrendingCocktails] = useState([]);
+  const [trendingCocktails, setTrendingCocktails] = useState([]);
 
-	useEffect(() => {
-		getTrendingCocktails();
-	}, []);
+  useEffect(() => {
+    getTrendingCocktails();
+  }, []);
 
-	const getTrendingCocktails = async () => {
-
+  const getTrendingCocktails = async () => {
     const apiKey = apiKeyCocktail;
-		try {
-			const response = await axios.get(
-				`https://www.thecocktaildb.com/api/json/v2/${apiKey}/popular.php`
-			);
+    try {
+      const response = await axios.get(
+        `https://www.thecocktaildb.com/api/json/v2/${apiKey}/popular.php`
+      );
 
-			const data = response.data;
-			const cocktails = data.drinks.slice(0, 5);
-			setTrendingCocktails(cocktails);
-			console.log(cocktails);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+      const data = response.data;
+      const cocktails = data.drinks.slice(0, 4);
+      setTrendingCocktails(cocktails);
+      console.log(cocktails);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="recipe-grid">
